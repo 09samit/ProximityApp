ba8565dc7e4147fa9cbf5bed0c4f6ec07590fbbf
Amit Garg <09s.amitgarg@gmail.com>
2024-11-19 01:18:22 +0530

    Setup project

---
+++ ProximityApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ProximityApp
+//
+//  Created by Amit Garg on 18/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        self.view = ProximityView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showInfoAlert()
+    }
+    
+    func showInfoAlert() {
+        let alert = UIAlertController(title: "Info", message: "Tap any where in the screen to start.", preferredStyle: .alert)
+        alert.addAction(.init(title: "Cancel", style: .cancel))
+        present(alert, animated: true)
+    }
+}
+
